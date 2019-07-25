b64a5580ef5170d67a2ccd7ded8362a6a540e0f3
Arpan Bag <arpanbag1996@gmail.com>
2019-07-26 00:51:22 +0530

    Database - Post creation - Added basic functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
@@ -17,4 +18,8 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080, () => { console.log("Listening at port 8080") });
+mongoose.connect("mongodb+srv://arpan:arpanbag12@cluster0-o4or2.mongodb.net/social?retryWrites=true&w=majority")
+    .then(
+        app.listen(8080, () => { console.log("Listening at port 8080") })
+    )
+    .catch(err => console.log(err));
