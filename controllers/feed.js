c50c2a5c2c724c995cdff461ecfe21220396189a
Arpan Bag <arpanbag1996@gmail.com>
2019-07-28 00:08:08 +0530

    Implemented pagination functionality

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,9 +6,16 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-    Post.find()
+    const currentPage = req.query.page || 1;    //Get request page number, or use 1 as default
+    const postPerPage = 2;
+    let totalItems;
+    Post.find().countDocuments()
+        .then(count => {
+            totalItems = count;
+            return Post.find().skip((currentPage - 1) * postPerPage).limit(postPerPage);
+        })
         .then(posts => {
-            res.status(200).json({ message: "Feteched posts successfully.", posts: posts });
+            res.status(200).json({ message: "Feteched posts successfully.", posts: posts, totalItems: totalItems });
         })
         .catch(err => {
             if (!err.statusCode)
